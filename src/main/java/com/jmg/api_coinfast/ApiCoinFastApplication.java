9582ae8c20a8d4654b98940cc07249b1920bb2fb
Jose Maria Galoppo <josemgaloppo@gmail.com>
2026-02-11 09:56:01 -0300

    first commit

---
+++ src/main/java/com/jmg/api_coinfast/ApiCoinFastApplication.java
@@ -0,0 +1,13 @@
+package com.jmg.api_coinfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCoinFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCoinFastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmg/api_coinfast/ApiCoinFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmg.api_coinfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCoinFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
